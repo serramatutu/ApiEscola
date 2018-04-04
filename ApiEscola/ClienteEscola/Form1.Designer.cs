4144daecd171c8e930a57a5f639f6142a80f863e
FELIPE MARTINS ROMEIRO <u16164@academico.cotuca.unicamp.br>
2018-04-04 18:17:41 -0300

    Cliente fino

--- ApiEscola/ClienteEscola/Form1.Designer.cs
+++ ApiEscola/ClienteEscola/Form1.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgvProjeto = new System.Windows.Forms.DataGridView();
             this.btnConsultar = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.txtCodigo = new System.Windows.Forms.TextBox();
@@ -39,19 +39,36 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.desc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ano = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prof = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.al1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.al2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.al3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjeto)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvProjeto
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.dgvProjeto.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 8);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(275, 116);
-            this.dataGridView1.TabIndex = 0;
+            this.dgvProjeto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProjeto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.nome,
+            this.desc,
+            this.ano,
+            this.prof,
+            this.al1,
+            this.al2,
+            this.al3});
+            this.dgvProjeto.Location = new System.Drawing.Point(12, 8);
+            this.dgvProjeto.Name = "dgvProjeto";
+            this.dgvProjeto.Size = new System.Drawing.Size(849, 116);
+            this.dgvProjeto.TabIndex = 0;
             // 
             // btnConsultar
             // 
@@ -59,7 +76,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.btnConsultar.Location = new System.Drawing.Point(12, 134);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(275, 23);
+            this.btnConsultar.Size = new System.Drawing.Size(849, 23);
             this.btnConsultar.TabIndex = 1;
             this.btnConsultar.Text = "Consultar Projetos";
             this.btnConsultar.UseVisualStyleBackColor = true;
@@ -68,7 +85,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(293, 17);
+            this.label1.Location = new System.Drawing.Point(867, 17);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 13);
             this.label1.TabIndex = 2;
@@ -77,7 +94,7 @@
             // txtCodigo
             // 
             this.txtCodigo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtCodigo.Location = new System.Drawing.Point(339, 14);
+            this.txtCodigo.Location = new System.Drawing.Point(913, 14);
             this.txtCodigo.Name = "txtCodigo";
             this.txtCodigo.Size = new System.Drawing.Size(191, 20);
             this.txtCodigo.TabIndex = 3;
@@ -85,27 +102,29 @@
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(536, 12);
+            this.button1.Location = new System.Drawing.Point(1110, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Busca por Código";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button2.Location = new System.Drawing.Point(536, 88);
+            this.button2.Location = new System.Drawing.Point(1110, 88);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 23);
             this.button2.TabIndex = 7;
             this.button2.Text = "Busca por Nome";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // txtNome
             // 
             this.txtNome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtNome.Location = new System.Drawing.Point(339, 90);
+            this.txtNome.Location = new System.Drawing.Point(913, 90);
             this.txtNome.MaxLength = 100;
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(191, 20);
@@ -115,7 +134,7 @@
             // 
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(293, 93);
+            this.label2.Location = new System.Drawing.Point(867, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 5;
@@ -124,7 +143,7 @@
             // button3
             // 
             this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button3.Location = new System.Drawing.Point(536, 41);
+            this.button3.Location = new System.Drawing.Point(1110, 41);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(118, 23);
             this.button3.TabIndex = 8;
@@ -134,7 +153,7 @@
             // button4
             // 
             this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.Location = new System.Drawing.Point(536, 134);
+            this.button4.Location = new System.Drawing.Point(1110, 134);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(118, 23);
             this.button4.TabIndex = 9;
@@ -144,18 +163,58 @@
             // button5
             // 
             this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.button5.Location = new System.Drawing.Point(412, 134);
+            this.button5.Location = new System.Drawing.Point(986, 134);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(118, 23);
             this.button5.TabIndex = 10;
             this.button5.Text = "Inserir Projeto";
             this.button5.UseVisualStyleBackColor = true;
             // 
+            // id
+            // 
+            this.id.HeaderText = "ID";
+            this.id.Name = "id";
+            // 
+            // nome
+            // 
+            this.nome.HeaderText = "Nome";
+            this.nome.Name = "nome";
+            // 
+            // desc
+            // 
+            this.desc.HeaderText = "Descricao";
+            this.desc.Name = "desc";
+            // 
+            // ano
+            // 
+            this.ano.HeaderText = "Ano";
+            this.ano.Name = "ano";
+            // 
+            // prof
+            // 
+            this.prof.HeaderText = "Professor";
+            this.prof.Name = "prof";
+            // 
+            // al1
+            // 
+            this.al1.HeaderText = "Aluno 1";
+            this.al1.Name = "al1";
+            // 
+            // al2
+            // 
+            this.al2.HeaderText = "Aluno 2";
+            this.al2.Name = "al2";
+            // 
+            // al3
+            // 
+            this.al3.HeaderText = "Aluno 3";
+            this.al3.Name = "al3";
+            // 
             // frmCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 169);
+            this.ClientSize = new System.Drawing.Size(1240, 169);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -166,10 +225,10 @@
             this.Controls.Add(this.txtCodigo);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnConsultar);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.dgvProjeto);
             this.Name = "frmCliente";
             this.Text = "Cliente Escola";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProjeto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -177,7 +236,7 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dgvProjeto;
         private System.Windows.Forms.Button btnConsultar;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtCodigo;
@@ -188,6 +247,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn desc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ano;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prof;
+        private System.Windows.Forms.DataGridViewTextBoxColumn al1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn al2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn al3;
     }
 }
 
--- ApiEscola/ClienteEscola/Form1.cs
+++ ApiEscola/ClienteEscola/Form1.cs
@@ -4,17 +4,109 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ApiEscola.Models;
+using Newtonsoft.Json;
 
 namespace ClienteEscola
 {
     public partial class frmCliente : Form
     {
+        private string URI = "http://localhost:49442/api/projetos/";
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = new HttpResponseMessage();
+
         public frmCliente()
         {
             InitializeComponent();
+        }
+
+        public async void getByName(string nome)
+        {
+            dgvProjeto.Rows.Clear();
+            /*
+            * Método HttpClient.GetAsync (Uri)
+            *
+            * Envia uma requisição GET para o URI especificado
+            * com uma operação assíncrona.
+            */
+            //O objeto response(HttpResponseMessage) recebe a resposta do envio de requisição ao endereço URI
+            response = await client.GetAsync(URI + $"getbyname/{nome}");
+            //Se o envio de requisição fdor atendido
+            if (response.IsSuccessStatusCode)
+            {
+                /*
+                * Propriedade HttpResponseMessage.Content
+                * Obtém ou define o conteúdo de uma mensagem de resposta HTTP.
+                *
+                * Para criar código assíncrono usamos as palavras chaves async
+                * e await onde por padrão um método modificado por uma palavra-chave
+                * async contém pelo menos uma expressão await.
+                */
+                //A variável AlunoJsonString recebe o resultado da requisição
+                var ProjetoJsonString = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<List<Projeto>>(ProjetoJsonString);
+                for (int i = 0; i < result.Count; i++)
+                {
+                    Projeto p = result[i];
+                    dgvProjeto.Rows.Add(p.Id, p.Nome, p.Descricao, p.Ano, p.Professor.Nome);
+                    for (int j = 0; j < p.Alunos.Count; j++)
+                        dgvProjeto.CurrentRow.Cells[5 + j].Value = p.Alunos[j].Nome;
+                }
+
+            }
+            else
+                MessageBox.Show($"Não foi possível obter o projeto com o nome {nome}");
+        }
+
+        private async void getById (string guid)
+        {
+            dgvProjeto.Rows.Clear();
+            /*
+            * Método HttpClient.GetAsync (Uri)
+            *
+            * Envia uma requisição GET para o URI especificado
+            * com uma operação assíncrona.
+            */
+            //O objeto response(HttpResponseMessage) recebe a resposta do envio de requisição ao endereço URI
+            response = await client.GetAsync(URI + $"getbyid/{guid}");
+            //Se o envio de requisição fdor atendido
+            if (response.IsSuccessStatusCode)
+            {
+                /*
+                * Propriedade HttpResponseMessage.Content
+                * Obtém ou define o conteúdo de uma mensagem de resposta HTTP.
+                *
+                * Para criar código assíncrono usamos as palavras chaves async
+                * e await onde por padrão um método modificado por uma palavra-chave
+                * async contém pelo menos uma expressão await.
+                */
+                //A variável AlunoJsonString recebe o resultado da requisição
+                var ProjetoJsonString = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<Projeto>(ProjetoJsonString);
+                Projeto p = result;
+                dgvProjeto.Rows.Add(p.Id, p.Nome, p.Descricao, p.Ano, p.Professor.Nome);
+                for (int j = 0; j < p.Alunos.Count; j++)
+                    dgvProjeto.CurrentRow.Cells[5 + j].Value = p.Alunos[j].Nome;
+
+            }
+            else
+                MessageBox.Show($"Não foi possível obter o projeto com o código {guid}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            getByName(txtNome.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            getById(txtCodigo.Text);
         }
     }
 }
