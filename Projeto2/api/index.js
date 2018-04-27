f2672c09c09eac21fa359984af64715d3840c127
serramatutu <lucasvvop@hotmail.com>
2018-04-27 10:00:34 -0300

    Horarios de monitoria no BD e na api

--- Projeto2/api/index.js
+++ Projeto2/api/index.js
@@ -6,11 +6,42 @@ const Request = require('tedious').Request;
 
 const app = express();
 
-app.get('/monitoria', function(req, res) {
+app.get('/monitoria/monitores', function(req, res) {
     db.getConnection((err, conn) => {
         if (err) throw err;
 
         var r = new Request("SELECT * FROM ApiMonitores", (err, rowCount, rows) => {
+            conn.close();
+
+            if (err) {
+                console.log(err); 
+                res.status(500).send("Erro de servidor");   
+            }
+            else {
+                var rowsArray = [];
+                rows.forEach(cols => {
+                    var rowObj = {};
+                    cols.forEach(col => {
+                        if (col.isNull)
+                            rowObj[col.metadata.colName] = null
+                        else
+                            rowObj[col.metadata.colName] = col.value;
+                    });
+                    rowsArray.push(rowObj);
+                });
+                res.send(rowsArray)
+            }
+        });
+
+        conn.execSql(r);
+    });
+});
+
+app.get('/monitoria/horarios', function(req, res) {
+    db.getConnection((err, conn) => {
+        if (err) throw err;
+
+        var r = new Request("SELECT * FROM vw_HorariosMonitoria", (err, rowCount, rows) => {
             conn.close();
 
             if (err) {
